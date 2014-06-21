335724e27d85fce88f3909220560e5fe26c0bfa3
Nikita Krasnoyartsev <i@knone.ru>
2014-06-21 20:44:16 +0400

    create bundle structure

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace KNone\TranslateBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * Class Configuration
+ * @package KNone\TranslateBundle\DependencyInjection
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('k_none_translate');
+
+        return $treeBuilder;
+    }
+}
---
+++ KNoneTranslateBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace KNone\TranslateBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class KNoneTranslateBundle
+ * @package KNone\TranslateBundle
+ */
+class KNoneTranslateBundle extends Bundle
+{
+}
