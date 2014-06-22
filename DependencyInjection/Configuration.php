07752d1e4982bbc50225d7fd51b6362fa2b46cff
Nikita Krasnoyartsev <i@knone.ru>
2014-06-23 02:02:29 +0400

    configuration builder

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -8,6 +8,7 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 /**
  * Class Configuration
  * @package KNone\TranslateBundle\DependencyInjection
+ * @author Krasnoyartsev Nikita <i@knone.ru>
  */
 class Configuration implements ConfigurationInterface
 {
@@ -18,7 +19,15 @@ class Configuration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('k_none_translate');
-
+        $rootNode
+            ->children()
+                ->scalarNode('yandex_api_key')
+                ->end()
+                ->enumNode('default_provider')
+                    ->values(array('google_web', 'yandex_api'))
+                    ->defaultValue('google_web')
+                 ->end()
+            ->end();
         return $treeBuilder;
     }
 }
