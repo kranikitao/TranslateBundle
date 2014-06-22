f061e936bef4d779cfb302e6095f6daa7101a626
Nikita Krasnoyartsev <i@knone.ru>
2014-06-23 01:16:05 +0400

    added Yandex Api translator as provider

---
+++ Provider/YandexApiProvider.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace KNone\TranslateBundle\Provider;
+
+use KNone\TranslateBundle\Model\Translation;
+use KNone\TranslateBundle\Exception\TranslateException;
+use GuzzleHttp\Client;
+
+/**
+ * Class YandexApiProvider
+ * @package KNone\TranslateBundle\Provider
+ * @author Krasnoyartsev Nikita <i@knone.ru>
+ */
+class YandexApiProvider implements ProviderInterface
+{
+    /**
+     * @var Client
+     */
+    protected $client;
+
+    /**
+     * @var string
+     */
+    protected $key;
+
+    /**
+     * @var string
+     */
+    protected $url = 'https://translate.yandex.net/api/v1.5/tr.json/translate';
+
+    /**
+     * @param Client $client
+     * @param string $key
+     */
+    public function __construct(Client $client, $key)
+    {
+        $this->client = $client;
+        $this->key = $key;
+    }
+
+    /**
+     * @param string $text
+     * @param string $sourceLanguage
+     * @param string $resultLanguage
+     * @return Translation
+     */
+    public function translate($text, $sourceLanguage, $resultLanguage)
+    {
+        $url = $this->composeUrl($text, $sourceLanguage, $resultLanguage);
+        $data = $this->execute($url);
+        $language = explode('-', $data['lang']);
+        $translation = new Translation($text, implode(' ', (array)$data['text']), $language[0], $resultLanguage);
+
+        return $translation;
+    }
+
+    /**
+     * @param string $text
+     * @param string $sourceLanguage
+     * @param string $resultLanguage
+     * @return string
+     */
+    protected function composeUrl($text, $sourceLanguage, $resultLanguage)
+    {
+        $lang = $sourceLanguage . '-' . $resultLanguage;
+        if ($sourceLanguage === 'auto') {
+            $lang = $resultLanguage;
+        }
+        $parameters = array(
+            'key' => $this->key,
+            'text' => $text,
+            'lang' => $lang,
+            'format' => 'plain',
+            'options' => 0
+        );
+
+        return $this->url . '?' . http_build_query($parameters);
+    }
+
+    /**
+     * @param $url
+     * @return mixed
+     * @throws \KNone\TranslateBundle\Exception\TranslateException
+     */
+    protected function execute($url)
+    {
+        $response = $this->client->get($url);
+        if ($response->getStatusCode() > 200) {
+            throw new TranslateException();
+        }
+
+        return $response->json();;
+    }
+
+}
---
+++ Tests/Provider/YandexApiProviderTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace KNone\TranslateBundle\Test\Provider;
+
+use GuzzleHttp\Client;
+use KNone\TranslateBundle\Provider\YandexApiProvider;
+use GuzzleHttp\Message\ResponseInterface;
+use KNone\TranslateBundle\Model\Translation;
+
+class YandexApiProviderTest extends \PHPUnit_Framework_TestCase
+{
+    const KEY = 'key';
+    const SOURCE = 'привет мир';
+    const RESULT = "hello world";
+    const SOURCE_LANGUAGE = 'ru';
+    const RESULT_LANGUAGE = 'en';
+
+    /**
+     * @var YandexApiProvider
+     */
+    protected $translator;
+
+    /**
+     * @var ResponseInterface
+     */
+    protected $response;
+
+    /**
+     * @var Client
+     */
+    protected $client;
+
+    protected function setUp()
+    {
+        $this->response = \Phake::mock('GuzzleHttp\Message\ResponseInterface');
+
+        $this->client = \Phake::mock('GuzzleHttp\Client');
+        \Phake::when($this->client)->get(\Phake::anyParameters())->thenReturn($this->response);
+
+        $this->translator = new YandexApiProvider($this->client, self::KEY);
+    }
+
+    public function testTranslate()
+    {
+        \Phake::when($this->response)->getStatusCode()->thenReturn(200);
+        \Phake::when($this->response)->json()->thenReturn($this->getResponseArray());
+
+        $translation = $this->translator->translate(self::SOURCE, self::SOURCE_LANGUAGE, self::RESULT_LANGUAGE);
+
+        $this->assertEquals($this->getTranslation(), $translation);
+    }
+
+    /**
+     * @expectedException KNone\TranslateBundle\Exception\TranslateException
+     */
+    public function testTranslateException()
+    {
+        \Phake::when($this->response)->getStatusCode()->thenReturn(404);
+
+        $this->translator->translate(self::SOURCE, self::SOURCE_LANGUAGE, self::RESULT_LANGUAGE);
+
+    }
+
+    /**
+     * @return Translation
+     */
+    protected function getTranslation()
+    {
+        return new Translation(self::SOURCE, self::RESULT, self::SOURCE_LANGUAGE, self::RESULT_LANGUAGE);
+    }
+
+    protected function getResponseArray()
+    {
+        return array(
+            'code' => 200,
+            'lang' => self::SOURCE_LANGUAGE . '-' . self::RESULT_LANGUAGE,
+            'text' => array(self::RESULT)
+        );
+    }
+
+
+}
